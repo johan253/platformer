63c6dfc0f3b2a647302f8c83be63e118b8cfcc97
johan253 <131953029+johan253@users.noreply.github.com>
2023-05-15 13:06:13 -0700

    Added Inputs
    added basic keyboard and mouse inputs to the program

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,10 +3,18 @@ package main;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
 public class GamePanel extends JPanel{
     
-    public GamePanel() {
+    private MouseInputs mouseInputs;
 
+    public GamePanel() {
+        mouseInputs = new MouseInputs();
+        addKeyListener(new KeyboardInputs());
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
 
     public void paintComponent(Graphics g) {
