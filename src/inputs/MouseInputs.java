f352ec4a0dbb013113550bf478ede5b6460bae93
johan253 <131953029+johan253@users.noreply.github.com>
2023-05-15 13:20:55 -0700

    Added Mouse Listeners
    Added a mouse listener that the rectangle now follows

--- src/inputs/MouseInputs.java
+++ src/inputs/MouseInputs.java
@@ -4,7 +4,15 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
+import main.GamePanel;
+
 public class MouseInputs implements MouseListener, MouseMotionListener {
+
+    private GamePanel gamePanel;
+    public MouseInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
+
 
     @Override
     public void mouseDragged(MouseEvent e) {
@@ -13,7 +21,7 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        System.out.println("Mouse moved");
+        gamePanel.setRectPos(e.getX(), e.getY());
     }
 
     @Override
